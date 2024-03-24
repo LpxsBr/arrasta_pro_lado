a72364275464718c7371e471767b61c9b377d680
LpxsBr <anselmolopes.an@gmail.com>
2024-03-23 21:41:27 -0300

    fix: lint fixes to deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="flex flex-col items-center">
-        <h1 className="font-bold text-2xl">Ei, arrasta pro lado ➡️➡️➡️➡️ </h1>
+    <main className='flex min-h-screen flex-col items-center justify-center p-24'>
+      <div className='flex flex-col items-center'>
+        <h1 className='font-bold text-2xl'>Ei, arrasta pro lado ➡️➡️➡️➡️ </h1>
         <p>Em breve . . .</p>
       </div>
     </main>
